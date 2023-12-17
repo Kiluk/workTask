ba267cdf76eb853bfc376c017ad5b0d08718da4c
Kiluk <lukasz96karpinski@gmail.com>
2023-12-17 15:23:38 +0100

    first commit

---
+++ src/Wall.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.Optional;
+
+public class Wall implements Structure {
+    private List<Block> blocks;
+
+    public Optional<Block> findBlockByColor(String color) {
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'findBlockByColor'");
+    }
+
+    public List<Block> findBlocksByMaterial(String material) {
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'findBlocksByMaterial'");
+    }
+
+    public int count() {
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'count'");
+    }
+}
+
